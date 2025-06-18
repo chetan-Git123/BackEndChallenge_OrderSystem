87c633feb3b0409ea4c4b9711db88e278b19d0c6
chetan-Git123 <37156929+chetan-Git123@users.noreply.github.com>
2025-06-18 05:07:47 -0700

    Update IOrderStore.cs

--- Domain/IOrderStore.cs
+++ Domain/IOrderStore.cs
@@ -1,1 +1,14 @@
+using Cricut.Orders.Domain.Models;
+
+namespace Cricut.Orders.Domain
+{
+    public interface IOrderStore
+    {
+        Task<Order> SaveOrderAsync(Order order);
+        Task<Order?> GetOrderByIdAsync(int orderId);
+        Task<Order[]> GetAllOrdersAsync();
+        Task<Order[]> GetAllOrdersForCustomerAsync(int customerId);
+        Task DeleteOrderByIdAsync(int orderId);
+    }
+}
 
